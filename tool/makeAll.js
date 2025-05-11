b23e3fa0082df572d4494737ed2f87bf38767918
Taisuke Fukuno <fukuno@jig.jp>
2025-05-11 22:07:29 +0900

    added year on teams.csv & added app result all

--- tool/makeAll.js
+++ tool/makeAll.js
@@ -5,12 +5,16 @@ const dirs = [];
 for await (const f of Deno.readDir("../data")) {
   if (!f.isDirectory) continue;
   const fn = "../data/" + f.name + "/team.csv";
-  dirs.push(fn);
+  const year = f.name;
+  dirs.push({ fn, year });
 }
-dirs.sort();
-for await (const fn of dirs) {
+dirs.sort((a, b) => a.year - b.year);
+for await (const { fn, year } of dirs) {
   console.log(fn);
   const data = await CSV.fetchJSON(fn);
-  data.forEach(i => list.push(i));
+  data.forEach(i => {
+    i.year = year;
+    list.push(i)
+  });
 }
 await Deno.writeTextFile(`../data/team.csv`, CSV.stringify(list));
